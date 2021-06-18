6be16bc6010df2a47ed36b01680c0247b018d94f
Ratakondala Arun <ratakondalaarun@gmail.com>
2021-06-18 16:01:23 +0530

    ✨ added frontend

---
+++ frontend/js/blogs/blogs.js
@@ -0,0 +1,40 @@
+const blogs = document.querySelector('#blogs');
+
+/**
+ * fetches list of blogs from the main branch of my github repo
+ */
+async function getBlogs() {
+  try {
+    const url = 'https://api.github.com/repos/RatakondalaArun/RatakondalaArun/contents/blogs';
+    const contentsRes = await httpRequest({
+      method: 'GET',
+      url: url,
+      headers: { Accept: 'application/vnd.github.v3+json' },
+    });
+    return JSON.parse(contentsRes.data);
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+async function loadBlogs() {
+  try {
+    const files = await getBlogs();
+    for (const file of files) {
+      blogs.appendChild(Blog(file));
+    }
+    document.querySelector('#loading.loading-ani').remove();
+  } catch (error) {}
+}
+loadBlogs();
+
+function Blog(blog) {
+  const { name } = blog;
+  const blogEle = document.createElement('div');
+  blogEle.classList.add('fadein-ani');
+  blogEle.innerHTML = `
+  <a href="/blog.html?name=${name}"><h2>${name}</h2></a>
+  `;
+  return blogEle;
+}
