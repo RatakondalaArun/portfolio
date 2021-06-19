c09e3200b7afe1258c6b8aea72a426222a45e995
Ratakondala Arun <ratakondalaarun@gmail.com>
2021-06-19 15:08:29 +0530

    ✨ added search for blog

--- frontend/js/blogs/blogs.js
+++ frontend/js/blogs/blogs.js
@@ -1,5 +1,5 @@
-const blogs = document.querySelector('#blogs');
-
+const blogsEle = document.querySelector('#blogs');
+let loadedBlogs = [];
 /**
  * fetches list of blogs from the main branch of my github repo
  */
@@ -18,23 +18,64 @@ async function getBlogs() {
   }
 }
 
+function Blog(blog) {
+  const { name } = blog;
+  const blogEle = document.createElement('div');
+  blogEle.id = name;
+  blogEle.classList.add('fadein-ani', 'blog');
+  blogEle.innerHTML = `
+  <a href="blog.html?name=${name}"><h2>${name}</h2></a>
+  `;
+  return blogEle;
+}
+
+// load blogs
 async function loadBlogs() {
   try {
-    const files = await getBlogs();
+    const mock = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((v) => {
+      return { name: `${v}-helloworld` };
+    });
+    // const files = await getBlogs();
+    const files = mock;
     for (const file of files) {
-      blogs.appendChild(Blog(file));
+      blogsEle.appendChild(Blog(file));
     }
+    loadedBlogs = files;
     document.querySelector('#loading.loading-ani').remove();
   } catch (error) {}
 }
 loadBlogs();
 
-function Blog(blog) {
-  const { name } = blog;
-  const blogEle = document.createElement('div');
-  blogEle.classList.add('fadein-ani');
-  blogEle.innerHTML = `
-  <a href="blog.html?name=${name}"><h2>${name}</h2></a>
-  `;
-  return blogEle;
+/// attach search listener
+const searchEle = document.querySelector('#search');
+searchEle.oninput = (_) => filterSearchs(searchEle.value.trim());
+
+/**
+ *
+ * @param {String} searchKey
+ */
+function filterSearchs(searchKey) {
+  if (searchKey === '') {
+    for (const blog of loadedBlogs) {
+      const ele = document.getElementById(blog.name);
+      ele.classList.remove('matched');
+      ele.classList.remove('unmatched');
+    }
+    return;
+  }
+
+  let matches = 0;
+  for (const blog of loadedBlogs) {
+    const element = document.getElementById(blog.name);
+    if (blog.name.includes(searchKey)) {
+      element.classList.add('matched');
+      element.classList.remove('unmatched');
+      ++matches;
+    } else {
+      element.classList.remove('matched');
+      element.classList.add('unmatched');
+    }
+  }
+
+  document.getElementById('search-info').hidden = !(matches === 0);
 }
